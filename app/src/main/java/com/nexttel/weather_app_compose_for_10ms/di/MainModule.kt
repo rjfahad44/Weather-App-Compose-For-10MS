3af7b005819a913ebfad7bec2f556bc34b15bb7a
Fahad <rjfahad44@gmail.com>
2024-11-28 22:22:38 +0600

    Add di modules for weather API call.

--- app/src/main/java/com/nexttel/weather_app_compose_for_10ms/di/MainModule.kt
+++ app/src/main/java/com/nexttel/weather_app_compose_for_10ms/di/MainModule.kt
@@ -1,11 +1,18 @@
 package com.nexttel.weather_app_compose_for_10ms.di
 
+import com.nexttel.weather_app_compose_for_10ms.data.remote.WeatherApi
+import com.nexttel.weather_app_compose_for_10ms.data.repository.WeatherRepository
+import com.nexttel.weather_app_compose_for_10ms.data.repository.WeatherRepositoryImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MainModule {
-
+    @Singleton
+    @Provides
+    fun provideWeatherRepository(weatherApi: WeatherApi): WeatherRepository = WeatherRepositoryImpl(weatherApi)
 }
--- app/src/main/java/com/nexttel/weather_app_compose_for_10ms/di/NetworkModule.kt
+++ app/src/main/java/com/nexttel/weather_app_compose_for_10ms/di/NetworkModule.kt
@@ -1,11 +1,28 @@
 package com.nexttel.weather_app_compose_for_10ms.di
 
+import com.nexttel.weather_app_compose_for_10ms.data.remote.WeatherApi
+import com.nexttel.weather_app_compose_for_10ms.utils.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providerRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(OkHttpClient.Builder().build())
+        .build()
+
+    @Singleton
+    @Provides
+    fun providerWeatherApi(retrofit: Retrofit): WeatherApi = retrofit.create(WeatherApi::class.java)
 
 }
